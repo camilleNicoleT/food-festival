5ff898610a3e40b07328264e5a4631594a1a79bb
camilleNicole <camilleht@gmail.com>
2022-04-24 17:45:10 -0400

    seperation of concerns

---
+++ assets/js/events.js
@@ -0,0 +1,6 @@
+require("bootstrap");
+const createEl = require("./domMethods");
+const createLoremIpsum = require("./helpers");
+
+
+if (window.location.href.indexOf("event") > -1) {...}  
---
+++ assets/js/schedule.js
@@ -0,0 +1,6 @@
+require("bootstrap");
+const createEl = require("./domMethods");
+const { createLoremIpsum, dateConverter } = require("./helpers");
+
+
+if (window.location.href.indexOf("schedule") > -1) {...}
